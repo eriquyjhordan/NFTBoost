4af581597d1bbdca0250e6614c86dbf1f53aca6a
Eriquy <eriquy@gmail.com>
2024-11-15 22:26:14 -0300

    fix: priority load on header

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -9,7 +9,7 @@ export default function Header() {
     <header className="absolute top-0 right-0 w-full">
       <div className="container flex items-center justify-between mt-6 lg:mt-5">
         <Link href="/">
-          <Image src={nftLogo} alt="nft logo" />
+          <Image src={nftLogo} alt="nft logo" priority />
         </Link>
         <nav className="hidden lg:block space-x-12">
           <Link
@@ -43,7 +43,7 @@ export default function Header() {
           className="py-3 px-6 w-fit hidden lg:block"
         />
         <button className="p-2 bg-white/[3%] border border-white/5 lg:hidden rounded">
-          <Image src={hamburgerMenu} alt="hamburger menu" />
+          <Image src={hamburgerMenu} alt="hamburger menu" priority />
         </button>
       </div>
     </header>
