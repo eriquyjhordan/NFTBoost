bc4388ff98786366f5d28523e51f6f3eca320070
Eriquy <eriquy@gmail.com>
2024-11-15 22:12:58 -0300

    chore: try dynamic imports

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,21 +1,28 @@
 import BannerSection from '@/components/banner-section'
-import CarrouselCards from '@/components/carrousel-cards'
 import CollectorsSection from '@/components/collectors-selection'
 import Faq from '@/components/faq'
 import Hero from '@/components/hero'
 import { featureCards, monthHighlights, trends } from '@/lib/cards-data'
+import dynamic from 'next/dynamic'
+
+const DynamicCarrouselCards = dynamic(
+  () => import('@/components/carrousel-cards'),
+  {
+    loading: () => <p>Loading...</p>,
+  },
+)
 
 export default function Home() {
   return (
     <>
       <Hero />
-      <CarrouselCards
+      <DynamicCarrouselCards
         carrouselData={featureCards}
         subtitle="Destaque em Coleções"
         title="Coleções notáveis"
       />
       <div className="container w-full bg-white/5 h-[1.5px] mt-20 lg:mt-28" />
-      <CarrouselCards
+      <DynamicCarrouselCards
         carrouselData={monthHighlights}
         subtitle="Seleção Mensal"
         title="Destaque do Mês"
@@ -23,7 +30,7 @@ export default function Home() {
       <div className="container w-full bg-white/5 h-[1.5px] mt-20 lg:mt-28" />
       <CollectorsSection />
       <div className="container w-full bg-white/5 h-[1.5px] mt-20 lg:mt-28" />
-      <CarrouselCards
+      <DynamicCarrouselCards
         carrouselData={trends}
         subtitle="Seleção Mensal"
         title="Destaque do Mês"
